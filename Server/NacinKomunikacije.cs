b68f4cb6c7b6463b95c3b2ac131d5f79eb9b8b96
popovmilana <milanaapopov@gmail.com>
2026-01-26 22:13:13 +0100

    dodata klasa NacinKomunikacije

---
+++ Server/NacinKomunikacije.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+
+public class NacinKomunikacije
+{
+    public IPEndPoint klijentAdresa { get; set; }
+    public string algoritam { get; set; }
+    public string kljuc { get; set; }
+    public string poruka { get; set; }
+    public NacinKomunikacije(string algoritam, string kljuc, string poruka, IPEndPoint adresa = null)
+    {
+        this.algoritam = algoritam;
+        this.kljuc = kljuc;
+        this.poruka = poruka;
+        this.klijentAdresa = adresa;
+    }
+
+    public NacinKomunikacije() { }
+}
